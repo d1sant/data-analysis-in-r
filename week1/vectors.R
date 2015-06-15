3d62345137d32967741e050fc72a69843b7e2370
Dmitry Palaznik <dpalaznik@gmail.com>
2015-06-15 19:52:58 +0300

    Vector creation and usage

---
+++ week1/vectors.R
@@ -0,0 +1,12 @@
+1 : 67
+
+myVector1 <- 1 : 67
+myVector2 <- c(-32, 45, 67, 12.78, 129, 0, -65)
+
+myVector1[1]
+myVector1[3]
+myVector2[1]
+
+myVector2[c(1,2,3)]
+myVector2[1:3]
+myVector2[c(1,5,6,7,10)]
