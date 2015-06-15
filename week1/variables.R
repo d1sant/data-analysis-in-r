8db976db8f0f7d8e090dcc5a4d42a7cc232b4e93
Dmitry Palaznik <dpalaznik@gmail.com>
2015-06-15 19:37:14 +0300

    Simple variables in R and some operations

---
+++ week1/variables.R
@@ -0,0 +1,13 @@
+myVar1 <- 12
+myVar2 <- 23.11
+
+myVar1 + 100
+
+myVar1 + myVar2 - 12
+myVar3 <- myVar1^2 + myVar2^2
+
+myVar3 > 300
+myVar3 >= 680
+myVar1 == myVar2
+myVar1 != myVar2
+myNewVar <- myVar1 == myVar2
