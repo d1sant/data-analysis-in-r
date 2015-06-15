bfe251f7cbec3180bce825b186c5b0408f28f1fa
Dmitry Palaznik <dpalaznik@gmail.com>
2015-06-15 20:03:46 +0300

    Operations on vectors

--- week1/vectors.R
+++ week1/vectors.R
@@ -10,3 +10,13 @@ myVector2[1]
 myVector2[c(1,2,3)]
 myVector2[1:3]
 myVector2[c(1,5,6,7,10)]
+
+myVector1 + 10
+myVector2 + 56
+
+myVector2 == 0
+myVector1 > 30
+
+x <- 23
+myVector1 > 23
+myVector1 > x
