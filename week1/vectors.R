0dc5d75dd2f14c011f33a4fdac194016b2792406
Dmitry Palaznik <dpalaznik@gmail.com>
2015-06-16 18:02:38 +0300

    Filtering of vectors, lists and dataframes

--- week1/vectors.R
+++ week1/vectors.R
@@ -20,3 +20,29 @@ myVector1 > 30
 x <- 23
 myVector1 > 23
 myVector1 > x
+
+myVector2 > 0
+myVector2[myVector2 > 0]
+myVector2[myVector2 < 0]
+myVector2[myVector2 <= 0]
+myVector2[myVector2 == 0]
+
+myVector1[myVector1 > 20 & myVector1 < 30]
+myNumbers <- myVector1[myVector1 > 20 & myVector1 < 30]
+positiveNumbers <- myVector2[myVector2 > 0]
+
+v1 <- c(165, 178, 180, 181, 167, 178, 187, 167, 187)
+meanV1 <- mean(v1)
+v1[v1 > meanV1]
+greaterThanMean <- v1[v1 > meanV1]
+
+age <- c(16, 18, 22, 27)
+isMaried <- c(F, F, T, T)
+name <- c("Olga", "Maria", "Nastya", "Polina")
+data <- list(age, isMaried, name)
+data
+data[[1]][1]
+data[[2]][3]
+
+df <- data.frame(Name = name, Age = age, Status = isMaried)
+typeof(df)
