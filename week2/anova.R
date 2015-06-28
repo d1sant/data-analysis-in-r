f2f5425d26e4874b0b86236529c650d145701512
Dmitry Palaznik <dpalaznik@gmail.com>
2015-06-28 10:06:51 +0300

    ANOVA tasks 4-6

--- week2/anova.R
+++ week2/anova.R
@@ -60,3 +60,68 @@ summary(t1)
 # 2nd task
 t2 <- aov(yield ~ N + P + K, df)
 summary(t2)
+
+# Pairwise comparison
+ggplot(mydata, aes(x = food, y = price)) +
+  geom_boxplot()
+
+fit5 <- aov(price ~ food, mydata)
+summary(fit5)
+
+TukeyHSD(fit5)
+
+# 3rd task
+t3 <- aov(Sepal.Width ~ Species, iris)
+summary(t3)
+
+TukeyHSD(t3)
+
+# Repeated measures
+mydata2 <- read.csv('therapy_data.csv')
+str(mydata2)
+
+mydata2$subject <- as.factor(mydata2$subject)
+
+fit1 <- aov(well_being ~ therapy, mydata2)
+summary(fit1)
+fit1b <- aov(well_being ~ therapy + Error(subject/therapy), mydata2)
+summary(fit1b)
+
+fit2 <-  aov(well_being ~ therapy * price, mydata2)
+summary(fit2)
+
+ggplot(mydata2, aes(x = price, y = well_being)) +
+  geom_boxplot()
+
+fit2b <- aov(well_being ~ therapy * price + Error(subject/(therapy * price)), mydata2)
+summary(fit2b)
+
+ggplot(mydata2, aes(x = price, y = well_being)) +
+  geom_boxplot() +
+  facet_grid(~subject)
+
+fit3 <- aov(well_being ~ therapy * price * sex, mydata2)
+summary(fit3)
+
+fit3b <- aov(well_being ~ therapy * price * sex + Error(subject/(therapy * price)), mydata2)
+summary(fit3b)
+
+# 4th task
+df <- read.csv("Pillulkin.csv")
+str(df)
+df$patient <- as.factor(df$patient)
+t4 <- aov(temperature ~ pill, df)
+t4 <- aov(temperature ~ pill + Error(patient/pill), df)
+summary(t4)
+
+# 5th task
+t5 <- aov(temperature ~ pill * doctor + Error(patient/(pill * doctor)), df)
+summary(t5)
+
+# 6th task
+obj <- ggplot(ToothGrowth, aes(x = as.factor(dose), y = len, col = supp)) +  
+  stat_summary(fun.data = mean_cl_boot, geom = 'errorbar', width = 0.1) + 
+  stat_summary(fun.data = mean_cl_boot, geom = 'point', size = 3) + 
+  stat_summary(fun.data = mean_cl_boot, geom = 'line')
+
+obj
