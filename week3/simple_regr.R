17eb8b37f5eae628cb1b9e7b9c5f1e0367e40027
Dmitry Palaznik <dpalaznik@gmail.com>
2015-07-10 11:14:01 +0300

    Simple regression tasks 2-4 and reminder

--- week3/simple_regr.R
+++ week3/simple_regr.R
@@ -1,4 +1,5 @@
 setwd("~/IdeaProjects/My/data-analysis-in-r/week3")
+library(ggplot2)
 
 df <- mtcars
 df_numeric <- df[,c(1,3:7)]
@@ -73,3 +74,64 @@ ggplot(my_df, aes(cyl, mpg)) +
 df <- read.table("dataset_11508_12.txt", sep = " ")
 fit <- lm(V1 ~ V2, df)
 summary(fit)
+
+# 2nd task
+str(diamonds)
+df <- diamonds[diamonds$cut == "Ideal" & diamonds$carat == 0.46,]
+fit <- lm(price ~ depth, df)
+print(fit$coefficients)
+
+# 3rd task
+regr.calc <- function(x) {
+  if (cor.test(x[,1], x[,2], method = "pearson")$p.value < 0.05) {
+    fit <- lm(x[,1] ~ x[,2], x)
+    x$fit <- fit$fitted.values
+    return(x)
+  } else {
+    return("There is no sense in prediction")
+  }
+}
+
+my_df <- iris[,1:2]
+regr.calc(my_df)
+
+my_df <- iris[,c(1,4)]
+regr.calc(my_df)
+
+# 4th task
+my_plot <- ggplot(iris, aes(Sepal.Width, Petal.Width, col = factor(Species))) +
+  geom_point() +
+  geom_smooth()
+
+# !!! REMINDER !!!
+
+cor.test(mtcars$mpg, mtcars$disp) # Расчет корреляции Пирсона 
+
+cor.test(~ mpg + disp, mtcars) # запись через формулу
+
+cor.test(mtcars$mpg, mtcars$disp, method = "spearman") # Расчет корреляции Спирмена 
+
+cor.test(mtcars$mpg, mtcars$disp, method = "kendall") # Расчет корреляции Кендала 
+
+cor(iris[, -5]) # построение корреляционной матрицы
+
+fit <- lm(mpg ~ disp, mtcars) # построение линейной регрессии 
+
+fit$coefficients # коэффициенты регрессии 
+
+fit$fitted.values # предсказанные значения зависимой переменной
+
+library(coin)
+spearman_test(~ mpg + disp, mtcars)
+
+ggplot(mtcars, aes(mpg, disp, col = factor(am)))+
+  geom_point()+
+  geom_smooth()
+
+ggplot(mtcars, aes(mpg, disp))+
+  geom_point(aes(col = factor(am)))+
+  geom_smooth()
+
+ggplot(mtcars, aes(mpg, disp))+
+  geom_point()+
+  geom_smooth(aes(col = factor(am)))
